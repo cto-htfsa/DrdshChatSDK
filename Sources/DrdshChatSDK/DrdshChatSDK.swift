207551d4fa884cdd6c952739ada26ea5c7a93d7e
Gaurav Gudaliya <whollysoftware@gmail.com>
2023-11-28 10:38:37 +0530

    no message

--- Sources/DrdshChatSDK/DrdshChatSDK.swift
+++ Sources/DrdshChatSDK/DrdshChatSDK.swift
@@ -307,13 +307,10 @@ public extension UIColor {
     }
     convenience init(hexString: String, alpha: CGFloat = 1.0) {
         var hexFormatted: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-
+        
         if hexFormatted.hasPrefix("#") {
             hexFormatted = String(hexFormatted.dropFirst())
         }
-
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-
         var rgbValue: UInt64 = 0
         Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
 
